c6cbf9d36bf2cbe4f90a7bb30d3bf36fb0bb0ee5
Praveen PL <praveen.pl03@gmail.com>
2026-07-29 16:12:22 +0530

    ok

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow,Menu } = require('electron');
 const express = require('express');
 const path = require('node:path');
 
@@ -20,7 +20,7 @@ const distPath = path.join(
   webApp.use((request, response) => {
     response.sendFile(path.join(distPath, 'index.html'));
   });
-
+Menu.setApplicationMenu(null);
   return new Promise((resolve) => {
     localServer = webApp.listen(0, '127.0.0.1', () => {
       const { port } = localServer.address();
